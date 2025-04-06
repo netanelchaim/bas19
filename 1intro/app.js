26f6263b670ce4f64571483194879847975c4529
netanelchaim <netanelchaim0@gmail.com>
2025-04-06 14:50:17 +0300

    netanel

---
+++ 1intro/app.js
@@ -0,0 +1,15 @@
+// output
+console.log("hello world ")
+// let y=5;constt=5;
+const x = 5;
+console.log(x);
+let firstname ="ntanel"
+let age = 6;
+console.log(firstname);
+console.log(firstname +" is "+age+" years old ")
+let x1= Math.random()
+console.log(x1)
+let y1 =Math.random()*100;
+console.log(y1)
+let t1=Math.floor(Math.random()*100)
+console.log(t1)
